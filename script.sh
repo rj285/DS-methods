637d17f56f9674fc715a25df05a25f19230a84c8
Rohit Joseph <rohitjosephrhea285@gmail.com>
2023-12-29 13:46:45 +0530

    Data structure methods

--- script.sh
+++
@@ -1,7 +0,0 @@
-echo "# DS-methods" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/rj285/DS-methods.git
-git push -u origin main
